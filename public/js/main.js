30dde2aefef5637e0463fa8b5735b8f9852eb396
markrajk <marko88rajkovic@gmail.com>
2020-05-21 13:51:09 +0200

    features page added 01 & new header functionality

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ var menuToggle = document.querySelectorAll('.menu-toggle');
 var header = document.querySelector('header');
 var nav = document.querySelector('.nav');
 var burger = nav.querySelector('.burger');
+var toolbar = document.querySelector('.toolbar');
+var body = document.querySelector('body');
+var subheader = document.querySelector('.subheader-wrapper');
 
 console.log('HELLO IE!');
 
@@ -24,11 +27,63 @@ for (let i = 0; i < menuToggle.length; i++) {
     }, 500);
     if (nav.classList.contains('onscreen')) {
       //window.scrollTo(0, 0);
+      enableScrolling();
       nav.classList.remove('onscreen');
       burger.innerHTML = '<i class="fas fa-bars show-md"></i>';
     } else {
+      disableScrolling();
       nav.classList.add('onscreen');
       burger.innerHTML = '<i class="fas fa-times show-md"></i>';
     }
   });
+}
+
+var scrollPositionY = window.scrollY;
+var scrollDown = false;
+var scrollEnabled = true;
+
+window.addEventListener('scroll', function (e) {
+  if (!scrollEnabled) return;
+  if (window.scrollY > scrollPositionY) {
+    scrollPositionY = window.scrollY;
+    scrollDown = true;
+  } else {
+    scrollPositionY = window.scrollY + 1;
+    scrollDown = false;
+  }
+
+  if (scrollDown) {
+    body.style.paddingTop = '7.05rem';
+    header.style.display = 'none';
+    subheader.style.top = '0';
+    subheader.style.boxShadow = '0 3px 5px 0 rgba(0,0,0,.1)';
+    subheader.style.borderBottom = 'none';
+    console.log(scrollPositionY);
+  } else {
+    body.style.paddingTop = '0';
+    header.style.display = 'block';
+    subheader.style.top = '7.05rem';
+    console.log(scrollPositionY);
+  }
+
+  if (scrollPositionY < 10) {
+    subheader.style.boxShadow = 'none';
+    subheader.style.borderBottom = '1.5px solid #F4F4F4';
+  }
+
+  console.log(scrollPositionY);
+});
+
+function disableScrolling() {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
+  scrollEnabled = false;
+}
+
+function enableScrolling() {
+  window.onscroll = function () {};
+  scrollEnabled = true;
 }
